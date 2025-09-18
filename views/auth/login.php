7e5ee7937cbbeffdb64b45d1ce85d347b82db388
den_pras <pras.rubicteam@gmail.com>
2025-09-19 02:33:54 +0700

    sessi

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../config/auth.php';
+require_once __DIR__ . '/../../config/browser_auth.php';
 
 $error = '';
 
--- views/member/profile.php
+++ views/member/profile.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '/../../config/auth.php';
+require_once __DIR__ . '/../../config/browser_auth.php';
 
 // Ensure user is logged in
 if (!isLoggedIn()) {
